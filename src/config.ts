6d39d5b99227be0ebcd7cff6d57dcd0849799c4c
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-12 21:30:53 +0700

    Feat: Update Layout

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+export const SITE = {
+  name: "Sistem Peminjaman",
+
+  origin: "https://sistem-peminjaman.vercel.app",
+  basePathname: "/",
+  trailingSlash: false,
+
+  title: "Sistem Peminjaman | Kelola dan Pantau Peminjaman di Kampus",
+  description:
+    "Sistem peminjaman adalah sebuah sistem yang digunakan untuk mengelola peminjaman di kampus.",
+  applicationName: "Sistem Peminjaman",
+  keywords: ["sistem", "peminjaman", "kelola", "peminjaman", "kampus"],
+  authors: [
+    {
+      name: "Ikram Maulana",
+      url: "https://ikram-maulana.tech",
+    },
+  ],
+  creator: "Ikram Maulana",
+  themeColor: "#F9F5EB",
+};
