daa02f328abdb5c5f59bcd54637f3a622e0a8fb7
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-14 19:10:41 +0700

    Feat: Update Dashboard Statuses

--- src/app/(authenticated)/dashboard/statuses/page.tsx
+++ src/app/(authenticated)/dashboard/statuses/page.tsx
@@ -5,31 +5,31 @@ import { notFound } from "next/navigation";
 import { FC, Suspense } from "react";
 
 const page: FC = async () => {
-    const user = await currentUser();
-    const role = user?.publicMetadata.role;
+  const user = await currentUser();
+  const role = user?.publicMetadata.role;
 
-    if (!role) {
-        return notFound();
-    }
+  if (!role) {
+    return notFound();
+  }
 
-    return (
-        <>
-            <div className="flex flex-col mb-6 space-y-2 md:flex-row md:items-center md:justify-between">
-                <div className="flex flex-col">
-                    <h2 className="text-3xl font-bold tracking-tight">Master Statuses</h2>
-                    <p className="text-gray-500">
-                        Fitur ini digunakan untuk mengelola jenis status peminjaman buku dan ruangan
-                    </p>
-                </div>
-                <div className="flex items-center space-x-2">
-                    {role === "admin" && <AddButton />}
-                </div>
-            </div>
-            <Suspense>
-                <Statuses />
-            </Suspense>
-        </>
-    );
+  return (
+    <>
+      <div className="flex flex-col mb-6 space-y-2 md:flex-row md:items-center md:justify-between">
+        <div className="flex flex-col">
+          <h2 className="text-3xl font-bold tracking-tight">Master Status</h2>
+          <p className="text-gray-500">
+            Daftar status peminjaman yang terdaftar di sistem.
+          </p>
+        </div>
+        <div className="flex items-center space-x-2">
+          <AddButton />
+        </div>
+      </div>
+      <Suspense>
+        <Statuses />
+      </Suspense>
+    </>
+  );
 };
 
 export default page;
