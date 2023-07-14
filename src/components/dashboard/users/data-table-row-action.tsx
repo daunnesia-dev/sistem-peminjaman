f27371c464abb8296ff9500e82a8d74462d657bf
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-14 10:24:50 +0700

    Feat: Implement Delete User List

--- src/components/dashboard/users/data-table-row-action.tsx
+++ src/components/dashboard/users/data-table-row-action.tsx
@@ -1,15 +1,17 @@
 "use client";
 
+import DeleteButton from "@/components/dashboard/users/delete-button";
+import { createUsersProps } from "@/lib/validator/dashboard/users/api";
 import { Button } from "@/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuShortcut,
+  DropdownMenuGroup,
   DropdownMenuTrigger,
 } from "@/ui/dropdown-menu";
 import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 import { Row } from "@tanstack/react-table";
+import { useState } from "react";
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>;
@@ -18,9 +20,12 @@ interface DataTableRowActionsProps<TData> {
 export function DataTableRowActions<TData>({
   row,
 }: DataTableRowActionsProps<TData>) {
+  const user = createUsersProps.parse(row.original);
+  const [open, setOpen] = useState(false);
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <DropdownMenu open={open} onOpenChange={setOpen}>
+      <DropdownMenuTrigger asChild onClick={() => setOpen(!open)}>
         <Button
           variant="ghost"
           className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
@@ -30,10 +35,9 @@ export function DataTableRowActions<TData>({
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[160px]">
-        <DropdownMenuItem>
-          Delete
-          <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
-        </DropdownMenuItem>
+        <DropdownMenuGroup onClick={() => setOpen(true)}>
+          <DeleteButton id={user.id} name={user.fullName} setOpen={setOpen} />
+        </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   );
