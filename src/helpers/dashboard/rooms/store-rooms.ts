17a76cc7c1203d72ad56d3524cb2cf3eaf134893
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-14 19:40:19 +0700

    Feat: Update Store Rooms Request

--- src/helpers/dashboard/rooms/store-rooms.ts
+++ src/helpers/dashboard/rooms/store-rooms.ts
@@ -1,16 +1,28 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { roomFormSchema } from "@/lib/validator/dashboard/rooms/api";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { z } from "zod";
 
 export const storeRooms = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  return useMutation(async (newData: z.infer<typeof roomFormSchema>) => {
-    const { data } = await axios.post("/api/dashboard/rooms/store", newData, {
-      headers: {
-        "Cache-Control": "no-store",
+  const queryClient = useQueryClient();
+  const mutation = useMutation(
+    async (newData: z.infer<typeof roomFormSchema>) => {
+      const { data } = await axios.post("/api/dashboard/rooms/store", newData, {
+        headers: {
+          "Cache-Control": "no-store",
+        },
+      });
+      return data.data;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ["rooms"],
+        });
       },
-    });
-    return data.data;
-  });
+    }
+  );
+
+  return mutation;
 };
