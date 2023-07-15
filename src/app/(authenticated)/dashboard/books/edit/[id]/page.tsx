72ebdcfc926eac9cac755bc7f0dd4cf70ee17464
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-15 14:17:25 +0700

    Feat: Implement Detail Books

---
+++ src/app/(authenticated)/dashboard/books/edit/[id]/page.tsx
@@ -0,0 +1,37 @@
+import EditBook from "@/components/dashboard/books/edit-book";
+import { Card, CardContent } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { currentUser } from "@clerk/nextjs";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+
+const page = async ({ params }: { params: { id: number } }) => {
+  const user = await currentUser();
+  const role = user?.publicMetadata.role;
+
+  if (!role || role !== "admin") {
+    return notFound();
+  }
+
+  return (
+    <>
+      <div className="flex flex-col mb-6 space-y-2">
+        <div className="flex flex-col">
+          <h2 className="text-3xl font-bold tracking-tight">Edit Buku</h2>
+          <p className="text-gray-500">
+            Halaman ini digunakan untuk mengedit buku.
+          </p>
+        </div>
+      </div>
+      <Card>
+        <CardContent className={cn("p-6")}>
+          <Suspense>
+            <EditBook id={params.id} />
+          </Suspense>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default page;
--- src/components/dashboard/books/data-table-row-action.tsx
+++ src/components/dashboard/books/data-table-row-action.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import DeleteButton from "@/components/dashboard/books/delete-button";
+import { cn } from "@/lib/utils";
 import { showBooksProps } from "@/lib/validator/dashboard/books/api";
 import { Button } from "@/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
+  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/ui/dropdown-menu";
 import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 import { Row } from "@tanstack/react-table";
+import Link from "next/link";
 import { useState } from "react";
 
 interface DataTableRowActionsProps<TData> {
@@ -36,6 +39,11 @@ export function DataTableRowActions<TData>({
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[160px]">
         <DropdownMenuGroup onClick={() => setOpen(true)}>
+          <Link href={`/dashboard/books/edit/${books.id}`}>
+            <DropdownMenuItem className={cn("hover:cursor-pointer")}>
+              Edit
+            </DropdownMenuItem>
+          </Link>
           <DeleteButton id={books.id} judul={books.judul} setOpen={setOpen} />
         </DropdownMenuGroup>
       </DropdownMenuContent>
