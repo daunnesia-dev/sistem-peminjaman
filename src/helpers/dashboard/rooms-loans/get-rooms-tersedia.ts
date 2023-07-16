3922ac8ef99d86c3f18e9c201aceec97ee948d7c
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-16 10:18:04 +0700

    Feat: Update Get Rooms Tersedia Request

--- src/helpers/dashboard/rooms-loans/get-rooms-tersedia.ts
+++ src/helpers/dashboard/rooms-loans/get-rooms-tersedia.ts
@@ -6,11 +6,14 @@ export const getRoomsTersedia = () => {
   return useQuery({
     queryKey: ["roomsTersedia"],
     queryFn: async () => {
-      const { data } = await axios.get("/api/dashboard/rooms-tersedia", {
-        headers: {
-          "Cache-Control": "no-store",
-        },
-      });
+      const { data } = await axios.get(
+        "/api/dashboard/rooms-loans/rooms-tersedia",
+        {
+          headers: {
+            "Cache-Control": "no-store",
+          },
+        }
+      );
 
       return data.data;
     },
