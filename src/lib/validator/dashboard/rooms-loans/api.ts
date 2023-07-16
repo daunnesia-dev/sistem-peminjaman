91b32f5d3f8c8ed177a9e2fbde9307fb5a710d6e
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-16 09:17:26 +0700

    Feat: Add Store Rooms Loans API and Request

--- src/lib/validator/dashboard/rooms-loans/api.ts
+++ src/lib/validator/dashboard/rooms-loans/api.ts
@@ -6,12 +6,32 @@ export const createRoomsLoansProps = z.object({
   tanggalPinjam: z.string(),
   tanggalKembali: z.string(),
   namaPeminjam: z.string(),
-  status: z.string(),
+  status: z.number(),
 });
+export const ApiRoomsLoansUpdateRequestValidator = createRoomsLoansProps;
+export const updateRoomsLoansAdminProps = z.object({
+  id: z.number(),
+  status: z.number(),
+});
+export const ApiRoomsLoansAdminUpdateRequestValidator =
+  updateRoomsLoansAdminProps;
+
+export const roomLoansFormSchema = z.object({
+  roomId: z.number(),
+  tanggalPinjam: z.string(),
+  tanggalKembali: z.string(),
+  keterangan: z.string(),
+});
+export const ApiRoomsLoansRequestValidator = roomLoansFormSchema;
 
 export const ApiRoomsLoansResponseValidator = z.object({
   error: z.string().nullable(),
   data: z.array(createRoomsLoansProps),
+});
+
+export const ApiRoomsLoansDeleteResponseValidator = z.object({
+  error: z.string().nullable(),
+  data: z.string(),
 });
 
 export type RoomsLoansProps = z.infer<typeof createRoomsLoansProps>;
