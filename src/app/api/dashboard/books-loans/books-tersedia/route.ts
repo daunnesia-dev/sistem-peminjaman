e57574a11825ed751b8402647dc56768f4422e31
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-18 06:42:23 +0700

    Feat: Add Books Tersedia API and Request

---
+++ src/app/api/dashboard/books-loans/books-tersedia/route.ts
@@ -0,0 +1,44 @@
+import { db } from "@/lib/db";
+import { ApiBooksLoansResponseValidator } from "@/lib/validator/dashboard/book-loans/api";
+import { auth } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export const GET = async (req: NextRequest) => {
+  const { userId } = auth();
+  if (!userId) {
+    return NextResponse.json("Unauthorized", { status: 401 });
+  }
+
+  try {
+    const books = await db.book.findMany({
+      where: {
+        stok: { gt: 0 },
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+    const response = ApiBooksLoansResponseValidator.parse({
+      error: null,
+      data: books,
+    });
+
+    return NextResponse.json(response, { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        {
+          error: error.issues,
+          data: null,
+        },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      { error: "Internal server error", data: null },
+      { status: 500 }
+    );
+  }
+};
