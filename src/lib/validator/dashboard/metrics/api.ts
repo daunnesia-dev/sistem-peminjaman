64e8280e01ffad3ed67eb5bdc4d31b4689f0b5f4
Ikram Maulana <ikram075@ummi.ac.id>
2023-07-26 08:34:23 +0700

    Feat: Update Get Metrics Validator

--- src/lib/validator/dashboard/metrics/api.ts
+++ src/lib/validator/dashboard/metrics/api.ts
@@ -4,6 +4,7 @@ export const ApiAdminResponseValidator = z.object({
   error: z.string().nullable(),
   data: z.object({
     user_count: z.number(),
+    bookLoc_count: z.number(),
     category_count: z.number(),
     statuses_count: z.number(),
     books_count: z.number(),
